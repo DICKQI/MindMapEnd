a5adfda1373d99ff82d3f17c6f02ddfbd2e38c3f
dickqi <dickqi87@gmail.com>
2020-06-29 02:08:06 +0800

    add

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 case $1 in
     "up")
     # 启动uwsgi服务
-    uwsgi --socket :8000 --buffer-size 32768 --module TeamworkMindmap.wsgi &
+    uwsgi --socket :8000 --buffer-size 32768 --daemonize /home/ubuntu/MindMap/map.log --module TeamworkMindmap.wsgi &
     ;;
     "down")
         # 关闭uwsgi服务
@@ -11,5 +11,18 @@ case $1 in
         pid=$(awk 'NR==2{print}' tmp);
         kill -9 $pid
         rm tmp
+    ;;
+    "restart")
+      # 关闭uwsgi服务
+        uport=8000
+        lsof -i :$uport | awk '{print $2}' > tmp
+        pid=$(awk 'NR==2{print}' tmp);
+        kill -9 $pid
+        rm tmp
+        # 启动uwsgi服务
+        uwsgi --socket :8000 --buffer-size 32768 --daemonize /home/ubuntu/MindMap/map.log --module TeamworkMindmap.wsgi &
+    ;;
+    "log")
+        tail /home/ubuntu/MindMap/map.log
     ;;
 esac
