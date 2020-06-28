bc3f812113e89563c5c4c64418a1d80416463e86
dickqi <dickqi87@gmail.com>
2020-06-28 14:45:38 +0800

    no message

--- App/MindMap/admin.py
+++ App/MindMap/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import MindMap, MindMapCoMember
+
 
 # Register your models here.
+
+@admin.register(MindMap)
+class AdminMindMap(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ['mapId', 'mapName', 'roomMaster', 'shareStatus', 'create_date', 'last_mod_date']
+
+
+@admin.register(MindMapCoMember)
+class AdminMindMapCoMember(admin.ModelAdmin):
+    list_per_page = 20
+    list_display = ['map', 'user', 'auth']
--- App/account/admin.py
+++ App/account/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import UserInfo
 
 # Register your models here.
+
+@admin.register(UserInfo)
+class AdminUserInfo(admin.ModelAdmin):
+    list_per_page = 50
+    list_display = ['nickname', 'email', 'join_date']
